7a9f74c05f8a1efb280226923359ebb77da59f49
SaamiiR24 <samir.ahouch@alumnos.upm.es>
2025-12-15 14:21:54 +0100

    estructura de boton rebotes y logica

--- project_1.srcs/sources_1/new/Rebotes.vhd
+++ project_1.srcs/sources_1/new/Rebotes.vhd
@@ -39,9 +39,33 @@ entity Rebotes is
     );
 end Rebotes;
 
+
 architecture Behavioral of Rebotes is
 
+    constant ESPERA_CICLOS : integer := 1000000;
+    
+    signal contador : integer range 0 to ESPERA_CICLOS := 0; -- contador del reloj acorde al reloj de la placa
+    signal estado_guardado : std_logic := '0'; 
+    
 begin
 
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            if (btn_in = estado_guardado) then
+                contador <= 0;
+            else
+                if (contador < ESPERA_CICLOS) then
+                    contador <= contador + 1;
+                else
+                    estado_guardado <= btn_in;
+                    contador <= 0;
+                end if;
+            end if;
+        end if;
+    end process;
+
+    -- la salida es siempre el estado "limpio" 
+    btn_out <= estado_guardado;
 
 end Behavioral;
